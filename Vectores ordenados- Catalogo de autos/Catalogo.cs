c2d14e43a4851f1334ac40f00917c9c4dfbd3b72
bflores4 <bflores4@ucol.mx>
2018-05-08 12:09:30 -0500

    practica vcetores desordenados

---
+++ Vectores ordenados- Catalogo de autos/Catalogo.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vectores_ordenados__Catalogo_de_autos
+{
+    class Catalogo
+    {
+        private Auto[] catalogoDeAutos;
+        private int _tamañoCatalogo;
+        private int _elementosRegistrados = 0;
+
+        public int elementosRegistrados
+        { get { return _elementosRegistrados; } }
+
+        public Catalogo(int tamañoCatalogo)
+        {
+            _tamañoCatalogo = tamañoCatalogo;
+            catalogoDeAutos = new Auto[tamañoCatalogo];
+        }
+
+        public void agregar(Auto autoParaAgregar)
+        {
+            catalogoDeAutos[_elementosRegistrados] = autoParaAgregar;
+            _elementosRegistrados++;
+        }
+
+        public Auto buscar(string placa)
+        {
+            Auto autoBuscado = null;
+            bool encontrado = false;
+
+            for(int i=0; i<_elementosRegistrados && encontrado == false; i++)
+            {
+
+                if (placa == catalogoDeAutos[i].placa)
+                {
+                    autoBuscado = catalogoDeAutos[i];
+                    encontrado = true;
+                    return autoBuscado;
+                }
+            }
+            return autoBuscado;
+        }
+
+        private int buscarPosicion(string placa)
+        {
+            int posicionBuscada = -1;
+            bool encontrado = false;
+
+            for (int i = 0; i < _elementosRegistrados && encontrado == false; i++)
+            {
+
+                if (placa == catalogoDeAutos[i].placa)
+                {
+                    posicionBuscada = i;
+                    encontrado = true;
+                    return posicionBuscada;
+                }
+            }
+            return posicionBuscada;
+        }
+
+        public void eliminar(string placa)
+        {
+            int posicion= buscarPosicion(placa);
+
+            if ( posicion!= -1)
+            {
+                for (int j = posicion; j < _elementosRegistrados - 1; j++)
+                {
+                    catalogoDeAutos[j] = catalogoDeAutos[j +1];
+                }
+                _elementosRegistrados--;
+            }
+        }
+
+        public bool PosibleInsertar()
+        {
+            bool posibleInsertar = false;
+            if (_elementosRegistrados < _tamañoCatalogo)
+            {
+                return posibleInsertar = true;
+            }
+            return posibleInsertar;
+        }
+
+        public void insertar(Auto autoParaInsertar , int posicion)
+        {
+            for (int i = _elementosRegistrados; i > posicion; i--)
+            {
+                catalogoDeAutos[i] = catalogoDeAutos[i - 1];
+            }
+
+            catalogoDeAutos[posicion] = autoParaInsertar;
+            _elementosRegistrados++;
+        }
+
+        public string listar() 
+        {
+            string listaAutos = "";
+            for (int i = 0; i < _elementosRegistrados; i++)
+            {
+                listaAutos += catalogoDeAutos[i].ToString();
+            }
+            return listaAutos;
+        }
+    }
+}
---
+++ Vectores ordenados- Catalogo de autos/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vectores_ordenados__Catalogo_de_autos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Catalogo catalogoCreado;
+        int tamañoCatalogo;
+        Auto autoCreado;
+
+        string placa;
+        string marca;
+        string modelo;
+        string telefono;
+        string año;
+
+        private void btnCreaCatslogo_Click(object sender, EventArgs e)
+        {
+            if (txtTamañoCatalogo.Text != "")
+            {
+                tamañoCatalogo = Convert.ToInt16(txtTamañoCatalogo.Text);
+                catalogoCreado = new Catalogo(tamañoCatalogo);
+            }
+            else
+            {
+                MessageBox.Show("El tamaño del catalógo no ha sido ingresado.");
+            }
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            if (txtTamañoCatalogo.Text != "")
+            {
+                if (catalogoCreado.elementosRegistrados < tamañoCatalogo)
+                {
+                    placa = txtPlaca.Text;
+                    marca = txtMarca.Text;
+                    modelo = txtModelo.Text;
+                    telefono = txtTelefono.Text;
+                    año = txtAño.Text;
+
+                    autoCreado = new Auto(placa, marca, modelo, telefono, año);
+
+                    catalogoCreado.agregar(autoCreado);
+                }
+                else
+                {
+                    MessageBox.Show("El catalógo se encuentra lleno, " +
+                        "no puede agregar más elemntos por el momento.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("El catalógo aun no ha sido creado");
+            }
+
+            txtPlaca.Text = "";
+            txtMarca.Text = "";
+            txtModelo.Text = "";
+            txtTelefono.Text = "";
+            txtAño.Text = "";
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)//listar
+        {
+            txtAutosParaMostrar.Text= catalogoCreado.listar();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            if(catalogoCreado.buscar(Convert.ToString(txtPlacaBuscar.Text))==null)
+                txtAutosParaMostrar.Text = "Auto no encontrado.";
+            else
+            {
+                txtAutosParaMostrar.Text = (catalogoCreado.buscar
+                                        (Convert.ToString(txtPlacaBuscar.Text))).ToString();
+            }
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            catalogoCreado.eliminar(Convert.ToString(txtPlacaBorrar.Text));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnInsertar_Click(object sender, EventArgs e)
+        {
+            if (catalogoCreado.PosibleInsertar() != false)
+            {
+                placa = txtPlaca.Text;
+                marca = txtMarca.Text;
+                modelo = txtModelo.Text;
+                telefono = txtTelefono.Text;
+                año = txtAño.Text;
+
+                autoCreado = new Auto(placa, marca, modelo, telefono, año);
+                int posicion = Convert.ToInt16(txtPosicionInsertar.Text);
+                catalogoCreado.insertar(autoCreado, posicion);
+            }
+            else
+            {
+                MessageBox.Show("El catalógo se encuentra lleno, no puede insertar otro elemento.");
+            }
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
